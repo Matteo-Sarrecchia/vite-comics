c49325321f4ce2b5c4129f0a278cd3d4b07d341a
Matteo Sarrecchia <morrea@hotmail.it>
2023-06-01 16:12:53 +0200

    section card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,48 @@
+<script >
+
+export default {
+    name: "AppCard",
+    props: {
+        details: Object
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="img-row">
+            <img :src="details.thumb" alt="">
+        </div>
+        <div class="text">{{ details.series }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.card {
+    min-height: 100px;
+    width: 180px;
+    margin: 10px auto;
+    border: 1px solid rgb(255, 255, 255);
+
+    .img-row {
+        width: 180px;
+        height: 180px;
+
+        img {
+            height: 180px;
+            width: 180px;
+            display: block;
+        }
+
+    }
+
+    .text {
+        margin: 10px;
+        text-align: center;
+        font-size: 15px;
+    }
+}
+</style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,21 @@
+<script >
+
+export default {
+    name: "AppJumbotron",
+}
+
+</script>
+
+<template>
+    <div class="jumbo"></div>
+</template>
+
+<style lang="scss">
+@use "../styles/partials/variables.scss" as *;
+
+.jumbo {
+    height: 300px;
+    background-image: url(../../public/img/jumbotron.jpg);
+    background-size: cover;
+}
+</style>
